3f254a8dfa4a0afd9135fa565a87f65052a07362
at-trinhnguyen2 <trinh.nguyen2@asiantech.vn>
2020-06-11 00:16:44 +0700

    create home ui list room
    Took 10 minutes

---
+++ app/src/main/java/com/ctr/hotelreservations/data/model/RoomInfo.kt
@@ -0,0 +1,16 @@
+package com.ctr.hotelreservations.data.model
+
+/**
+ * Created by at-trinhnguyen2 on 2020/06/06
+ */
+data class RoomInfo(
+    val id: Int,
+    val category: String,
+    val location: String,
+    val description: String,
+    val isLike: Boolean,
+    val title: String,
+    val price: Long,
+    val discount: Float,
+    val ratingStar: Int
+)
